eab6d70d0cf20b8da6a4d1be579c365701f871a4
Michał Rajzer <mmrajzer@gmail.com>
2019-03-19 18:07:29 +0100

    Carousel to fix

--- js/script.js
+++ js/script.js
@@ -5,12 +5,32 @@ var flkty = new Flickity( elem, {
   contain: true
 });
 
-// element argument can be a selector string
-//   for an individual element
+
+// carosuel made with mustache templates
+
+var mainCarousel = document.getElementsByClassName('main-carousel').innerHTML;
+var sliderCell = document.getElementById('slider-cell').innerHTML;
+
+Mustache.parse(sliderCell);
+
+var listItems = '';
+
+for (var i = 0; i < sliderData.length; i++){
+  console.log(sliderData);
+  listItems += Mustache.render(sliderCell, sliderData[i]);
+}
+
+mainCarousel.insertAdjacentHTML('beforeend', listItems);
+
+
+//Initialize flkty carosuel plugin
+
 var flkty = new Flickity( '.main-carousel', {
   // options
   hash: true,
 });
+
+// Add progress bar for the carousel
 
 var progressBar = document.querySelector('.progress-bar');
 
@@ -18,6 +38,8 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// Add reset button to return to first picture
 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.buttonReset');
